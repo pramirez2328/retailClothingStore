3514f98680f350faf99ea2f4d4b31c3233e7f329
Pedro Ramirez <prlara2000@gmail.com>
2024-11-01 16:53:56 -0500

    feat: find the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import Header from './components/Header/Header';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  return <div>hello world</div>;
+  // https://dummyjson.com/
+  const apiCall = async () => {
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
