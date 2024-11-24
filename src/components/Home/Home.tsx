65ef24a2ea2c692c3f4836f8a92dfb350af961e9
Pedro Ramirez <prlara2000@gmail.com>
2024-11-24 16:45:18 -0600

    feat: fix main page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,6 @@ import mainVideo from '../../assets/mainVideo.mp4';
 import clothes1 from '../../assets/clothes1.jpg';
 import clothes2 from '../../assets/clothes2.jpg';
 import clothes3 from '../../assets/clothes3.jpg';
-import logo from '../../assets/logo.png';
 import './Home.css';
 
 function Home() {
@@ -16,15 +15,20 @@ function Home() {
 
         <video className='full-screen-video' src={mainVideo} autoPlay muted loop></video>
       </section>
-      <section id='section-logo'>
-        <img id='logo' src={logo} alt='brand logo' />
-        <h1>Where Style Meets Comfort.</h1>
-      </section>
 
-      <section id='clothes-images'>
-        <img className='clothes-img' src={clothes1} alt='fancy clotes' />
-        <img className='clothes-img' src={clothes2} alt='fancy clotes' />
-        <img className='clothes-img' src={clothes3} alt='fancy clotes' />
+      <section>
+        <div className='section-text-img'>
+          <h1 className='section-text'>Where Style Meets Comfort.</h1>
+          <img className='section-img' src={clothes1} alt='fancy clotes' />
+        </div>
+        <div className='section-text-img'>
+          <h1 className='section-text'>Where Style Meets Comfort.</h1>
+          <img className='section-img' src={clothes2} alt='fancy clotes' />
+        </div>
+        <div className='section-text-img'>
+          <h1 className='section-text'>Where Style Meets Comfort.</h1>
+          <img className='section-img' src={clothes3} alt='fancy clotes' />
+        </div>
       </section>
     </>
   );
