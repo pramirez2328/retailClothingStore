60cc48cdfa3d76d67db80ca3797c885aa06d29f7
Pedro Ramirez <prlara2000@gmail.com>
2024-12-08 16:20:18 -0600

    fix: fix mobile on PDP and PLP

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -13,7 +13,7 @@ function Card({ product }: { product: Product }) {
     <div className='card' onClick={goToProductDetail} style={{ cursor: 'pointer' }}>
       <img className='product-image' src={product.images[0]} alt={product.title} />
       <h3 className='title'>{product.title}</h3>
-      <p className='product-info brand'>{product.brand}</p>
+      <p className='product-info brand'>{product.brand ? product.brand : 'Armany'}</p>
       <p className='product-info'>${product.price}</p>
       <p className='product-info'>{product.availabilityStatus}</p>
     </div>
